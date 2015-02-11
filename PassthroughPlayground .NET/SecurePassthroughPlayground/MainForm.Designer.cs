78dd8117869773beafe17711ce54d7cf9a16eafe
Shiva <shivakav@microsoft.com>
2015-02-11 10:56:45 -0500

    including the source of playground app

---
+++ PassthroughPlayground .NET/SecurePassthroughPlayground/MainForm.Designer.cs
@@ -0,0 +1,390 @@
+﻿namespace SecurePassthroughPlayground
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.button1 = new System.Windows.Forms.Button();
+            this.payload = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.url = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label5 = new System.Windows.Forms.Label();
+            this.certPath = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.instanceId = new System.Windows.Forms.TextBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.log = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.passThru = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.webUrl = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(378, 310);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Drop off -->";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // payload
+            // 
+            this.payload.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.payload.Location = new System.Drawing.Point(57, 19);
+            this.payload.Multiline = true;
+            this.payload.Name = "payload";
+            this.payload.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.payload.Size = new System.Drawing.Size(396, 285);
+            this.payload.TabIndex = 2;
+            this.payload.Text = resources.GetString("payload.Text");
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Payload";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(560, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Drop off Url";
+            // 
+            // url
+            // 
+            this.url.Location = new System.Drawing.Point(627, 41);
+            this.url.Name = "url";
+            this.url.Size = new System.Drawing.Size(307, 20);
+            this.url.TabIndex = 6;
+            this.url.Text = "https://sso-mutual-auth.parature.com/ext/ref/dropoff";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(107, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Client Cert .CER path";
+            // 
+            // certPath
+            // 
+            this.certPath.Location = new System.Drawing.Point(136, 19);
+            this.certPath.Name = "certPath";
+            this.certPath.Size = new System.Drawing.Size(326, 20);
+            this.certPath.TabIndex = 10;
+            this.certPath.Text = "c:\\ExportableSecurePassThroughCertFromCertStore.cer";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(468, 17);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(24, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(30, 74);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Your InstanceId";
+            // 
+            // instanceId
+            // 
+            this.instanceId.Location = new System.Drawing.Point(136, 71);
+            this.instanceId.Name = "instanceId";
+            this.instanceId.Size = new System.Drawing.Size(147, 20);
+            this.instanceId.TabIndex = 15;
+            this.instanceId.Text = "shivakavSecPass";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox3);
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(1410, 672);
+            this.splitContainer1.SplitterDistance = 460;
+            this.splitContainer1.TabIndex = 17;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.log);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox3.Location = new System.Drawing.Point(0, 339);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(460, 333);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Log";
+            // 
+            // log
+            // 
+            this.log.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.log.Location = new System.Drawing.Point(3, 16);
+            this.log.Multiline = true;
+            this.log.Name = "log";
+            this.log.Size = new System.Drawing.Size(454, 314);
+            this.log.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.payload);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(460, 339);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Test...";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(57, 310);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(118, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Pretty Print Payload";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.groupBox2);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.webBrowser1);
+            this.splitContainer2.Panel2.Controls.Add(this.panel1);
+            this.splitContainer2.Size = new System.Drawing.Size(946, 672);
+            this.splitContainer2.SplitterDistance = 97;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.passThru);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.url);
+            this.groupBox2.Controls.Add(this.instanceId);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.certPath);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(0, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(946, 97);
+            this.groupBox2.TabIndex = 17;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Settings";
+            // 
+            // passThru
+            // 
+            this.passThru.Location = new System.Drawing.Point(627, 67);
+            this.passThru.Name = "passThru";
+            this.passThru.Size = new System.Drawing.Size(307, 20);
+            this.passThru.TabIndex = 17;
+            this.passThru.Text = "https://demo.parature.com/ics/support/security2.asp ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(493, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 13);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Secure Passthrough URL";
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 33);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(946, 538);
+            this.webBrowser1.TabIndex = 1;
+            this.webBrowser1.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.webBrowser1_Navigated);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.webUrl);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(946, 33);
+            this.panel1.TabIndex = 0;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(899, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(44, 23);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "Go";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // webUrl
+            // 
+            this.webUrl.Location = new System.Drawing.Point(0, 6);
+            this.webUrl.Name = "webUrl";
+            this.webUrl.Size = new System.Drawing.Size(893, 20);
+            this.webUrl.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(7, 44);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(494, 12);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "* Install PFX on the machine you run this. Export a .CER of the certificate. Ente" +
+    "r path of it to attach the cert to the request";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1410, 672);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "MainForm";
+            this.Text = "Secure Passthrough Tester";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox payload;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox url;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox certPath;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox instanceId;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.TextBox log;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox webUrl;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TextBox passThru;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ PassthroughPlayground .NET/SecurePassthroughPlayground/MainForm.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace SecurePassthroughPlayground
+{
+    public partial class MainForm : Form
+    {
+        private const string instanceIdParamName = "ping.instanceId";
+
+        public MainForm()
+        {
+            InitializeComponent();
+        
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            log.Text = "";
+
+            try
+            {
+                var collection = new X509Certificate2Collection();
+                //collection.Import(certPath.Text, pass.Text, X509KeyStorageFlags.PersistKeySet);
+                collection.Import(certPath.Text);
+
+                //send this user data to the Parature DropOff Url (this is a server-side request)
+                var requestToPing = (HttpWebRequest)WebRequest.Create(url.Text);
+                requestToPing.Method = "POST";
+                requestToPing.PreAuthenticate = true;
+                requestToPing.ClientCertificates.Add(collection[0]);
+                requestToPing.Headers.Add(instanceIdParamName, instanceId.Text);
+
+                using (var sr = new StreamWriter(requestToPing.GetRequestStream()))
+                {
+                    sr.Write(payload.Text);
+                }
+
+                // read response from Parature and grab the REF parameter
+                WebResponse responseFromPing = requestToPing.GetResponse();
+                JToken refId;
+                using (var reader = new StreamReader(responseFromPing.GetResponseStream()))
+                {
+                    JObject.Parse(reader.ReadToEnd()).TryGetValue("REF", out refId);
+                }
+
+                Log("Drop Off was successful. Ref Id = " + refId);
+                Log("");
+
+                string PostDataStr = string.Format("refID={0}&instanceID={1}", refId, instanceId.Text);
+                byte[] PostDataByte = Encoding.UTF8.GetBytes(PostDataStr);
+                string AdditionalHeaders = "Content-Type: application/x-www-form-urlencoded" + Environment.NewLine;
+
+                webBrowser1.Navigate(passThru.Text, "", PostDataByte, AdditionalHeaders);
+                webUrl.Text = passThru.Text;
+
+                Log(string.Format("Post Data sent to {0}:", passThru.Text));
+                Log("");
+                Log(PostDataStr);
+
+                splitContainer2.Panel2.Enabled = true;
+            }
+            catch (Exception e1)
+            {
+                log.Text = "";
+                Log("Error: " + e1);
+            }
+        }
+
+        private void Log(string message)
+        {
+            log.AppendText(message);
+            log.AppendText(Environment.NewLine);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            certPath.Text = openFileDialog1.FileName;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            splitContainer2.Panel2.Enabled = false;
+        }
+
+        public string GetPrettyPrintedJson(string json)
+        {
+            dynamic parsedJson = JsonConvert.DeserializeObject(json);
+            return JsonConvert.SerializeObject(parsedJson, Formatting.Indented);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            payload.Text = GetPrettyPrintedJson(payload.Text);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(webUrl.Text);
+        }
+
+        private void webBrowser1_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            webUrl.Text = webBrowser1.Url.ToString();
+        }
+    }
+}
